b70ff0f6558b7a7b0d8203755c960ed72485030a
yusef <131935248+yusef800@users.noreply.github.com>
2024-05-11 22:45:48 -0500

    Routing freaking works

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import HomePage from "./pages/HomePage"
+import Inventory from "./pages/Inventory"
+import AboutUs from './pages/AboutUs';
+import ContactPage from './pages/ContactPage';
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <HomePage />
-      <Footer />
-    </>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/inventory" element={<Inventory/>} />
+          <Route path="/contact" element={<ContactPage/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Inventory = () => {
   return (
     <div>
-      
+      <h1>We are in inventory. W chat</h1>
     </div>
   )
 }
