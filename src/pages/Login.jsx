11727e0563be18b88ef79c9ac1bbe72ae4593ccd
yusef <yusef8000@gmail.com>
2024-05-23 12:29:42 -0500

    2FA has been setup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Helmet from 'react-helmet';
 import { getDocs, collection, query, where } from "firebase/firestore";
-import { db } from "../firebase";
+import { db, auth } from "../firebase";
+import { signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [isVerifying, setIsVerifying] = useState(false);
+  const [confirmationResult, setConfirmationResult] = useState(null);
   const navigate = useNavigate();
 
   const handleUsernameChange = (e) => {
@@ -17,20 +22,47 @@ function Login() {
     setPassword(e.target.value);
   };
 
+  const handleVerificationCodeChange = (e) => {
+    setVerificationCode(e.target.value);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
       const q = query(collection(db, "logins"), where("username", "==", username), where("password", "==", password));
       const querySnapshot = await getDocs(q);
-      
+
       if (!querySnapshot.empty) {
-        navigate("/admin-dashboard");
+        const userDoc = querySnapshot.docs[0].data();
+        setPhoneNumber(userDoc.phoneNumber);
+
+        // proceed to recaptcha verification if login correct
+        window.recaptchaVerifier = new RecaptchaVerifier(auth,'recaptcha-container', {
+          'size': 'invisible',
+          'callback': (response) => {
+          }
+        }, auth);
+
+        // confirm code with user and if correct proceed
+        const confirmationResult = await signInWithPhoneNumber(auth, userDoc.phoneNumber, window.recaptchaVerifier);
+        setConfirmationResult(confirmationResult);
+        setIsVerifying(true);
       } else {
         console.log("Invalid username or password");
       }
     } catch (error) {
       console.error("Error checking credentials: ", error);
+    }
+  };
+
+  const handleVerifyCode = async (e) => {
+    e.preventDefault();
+    try {
+      await confirmationResult.confirm(verificationCode);
+      navigate("/admin-dashboard");
+    } catch (error) {
+      console.error("Error verifying code: ", error);
     }
   };
 
@@ -44,33 +76,52 @@ function Login() {
           <img className="w-12 h-12 mx-auto mb-4" src="/lakesAutoSales.svg" alt="logo" />
           <h1 className="text-2xl font-bold text-white dark:text-white">Sign in to your account</h1>
         </div>
-        <form className="space-y-4" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="username" className="block mb-2 text-sm font-medium text-white dark:text-white">Username</label>
-            <input 
-              type="text" 
-              name="username" 
-              id="username" 
-              value={username} 
-              onChange={handleUsernameChange} 
-              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-              required 
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-white dark:text-white">Password</label>
-            <input 
-              type="password" 
-              name="password" 
-              id="password" 
-              value={password} 
-              onChange={handlePasswordChange} 
-              className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-              required 
-            />
-          </div>
-          <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-        </form>
+        {!isVerifying ? (
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="username" className="block mb-2 text-sm font-medium text-white dark:text-white">Username</label>
+              <input 
+                type="text" 
+                name="username" 
+                id="username" 
+                value={username} 
+                onChange={handleUsernameChange} 
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                required 
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-white dark:text-white">Password</label>
+              <input 
+                type="password" 
+                name="password" 
+                id="password" 
+                value={password} 
+                onChange={handlePasswordChange} 
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                required 
+              />
+            </div>
+            <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+            <div id="recaptcha-container"></div>
+          </form>
+        ) : (
+          <form className="space-y-4" onSubmit={handleVerifyCode}>
+            <div>
+              <label htmlFor="verificationCode" className="block mb-2 text-sm font-medium text-white dark:text-white">Verification Code</label>
+              <input 
+                type="text" 
+                name="verificationCode" 
+                id="verificationCode" 
+                value={verificationCode} 
+                onChange={handleVerificationCodeChange} 
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                required 
+              />
+            </div>
+            <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Verify Code</button>
+          </form>
+        )}
       </div>
     </section>
   );
