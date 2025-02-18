4226b440e36a3fd03149e25a3883f27e0a88bcf4
yusef <yusef8000@gmail.com>
2025-02-18 10:20:55 -0600

    connects to mongoDB online server

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const connect = require('./connect')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose
+
 
 app.get("/api", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World")
 });
 
-app.listen(5000, () => {console.log("Server is running on port 5000")});
+app.listen(5000, () => {
+  connect.connectToServer()
+  console.log("Server is running on port 5000")});
