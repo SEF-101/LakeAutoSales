a9cc4e67dd24c4f8bd40f33d6935f5ede5aac02f
yusef <yusef8000@gmail.com>
2025-02-23 19:54:00 -0600

    reduced logging on controllers

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -5,7 +5,7 @@ const getAllUsers = async (res) => {
   console.log('GET /users/all request received');
   let data = await User.find({});
   if (data.length > 0) {
-    console.log('Users found:', data);
+    console.log('Users found');
     res.json(data);
   } else {
     console.log('No users found');
@@ -19,7 +19,7 @@ const createUser = async (req, res) => {
   try {
     const newUser = new User(req.body);
     const savedUser = await newUser.save();
-    console.log('User saved:', savedUser);
+    console.log('User saved');
     res.status(201).json(savedUser);
   } catch (error) {
     console.error('Error saving user:', error.message);
@@ -33,7 +33,7 @@ const deleteUser = async (req, res) => {
   try {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
     if (deletedUser) {
-      console.log('User deleted:', deletedUser);
+      console.log('User deleted');
       res.json({ message: "User deleted successfully" });
     } else {
       console.log('No user found with id:', req.params.id);
