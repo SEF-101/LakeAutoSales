8e11f9f3d03d3ddca1d6f274fc71793310cd6fdd
yusef <yusef8000@gmail.com>
2025-03-10 20:09:17 -0500

    helment provider added

--- Frontend/src/pages/AdminDashboard.jsx
+++ Frontend/src/pages/AdminDashboard.jsx
@@ -1,11 +1,17 @@
 import AllListingsCard from "../components/AdminDash/AllListingsCard";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function AdminDashboard() {
   return (
+  <HelmetProvider>
+    <>
+      <Helmet>
+        <title>Lakes Auto Sales | Management Dashboard</title>
+      </Helmet>
     <div className="container mx-auto p-6 space-y-6">
       <div className="bg-gradient-to-r from-blue-800 to-black text-white p-10 rounded-lg shadow-lg text-center">
         <div className="flex flex-col items-center space-y-2">
-          <h1 className="text-4xl font-extrabold">Admin Dashboard</h1>
+          <h1 className="text-4xl font-extrabold">Management Dashboard</h1>
           <p className="text-lg text-gray-200">
             Manage vehicle listings and track inventory.
           </p>
@@ -14,6 +20,8 @@ function AdminDashboard() {
 
       <AllListingsCard />
     </div>
+    </>
+  </HelmetProvider>
   );
 }
 
