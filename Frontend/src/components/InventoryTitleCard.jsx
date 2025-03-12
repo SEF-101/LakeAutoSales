bbde4901a59ed132f6bdc8b13b9d05ef063696fc
yusef <yusef8000@gmail.com>
2025-03-12 16:34:38 -0500

    Update InventoryTitleCard.jsx

--- Frontend/src/components/InventoryTitleCard.jsx
+++ Frontend/src/components/InventoryTitleCard.jsx
@@ -1,12 +1,12 @@
 export default function InventoryTitleCard() {
     return (
         <div className="bg-gradient-to-r from-gray-900 to-gray-700 text-white p-10 rounded-lg shadow-lg text-center">
-        <div className="flex flex-col items-center space-y-2">
-        <h1 className="text-4xl font-extrabold">Our Inventory</h1>
-        <p className="text-lg text-gray-200">
-            Browse our selection of vehicles.
-        </p>
+            <div className="flex flex-col items-center space-y-2">
+                <h1 className="text-4xl font-extrabold">Our Inventory</h1>
+                <p className="text-lg text-gray-200">
+                    Browse our selection of vehicles.
+                </p>
+            </div>
         </div>
-    </div>
     );
 }
